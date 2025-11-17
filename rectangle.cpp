fbd97118f5f6ad03ae21e0f0106ca4812d62263b
ciemnowidzoliwier <oliwiergabryjeluk@gmail.com>
2025-11-17 10:02:01 +0000

    Rektangel 2B Gabryjeluk

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
-
+using namespace std;
 class Rectangle {
-
+    private:
+    double width;
+    double height;
+    public:
+    Rectangle(double w, double h)
+    : width(w), height(h){};
+    void setWidth(double w){
+        width=w;
+    }
+    void setheight(double h){
+        height=h;
+    }
+double getwidth() const{
+    return width;
+}
+double getheight()const{
+    return height;
+}
+double area()const{
+    return width*height;
+}
+double perimeter() const{
+    return 2*(width+height);
+}
+void display(double w, double h) const {
+		cout <<"Rectangle " << width << " x " << height << endl;
+		cout <<"Area " << area() << endl;
+		cout <<"Perimeter "<< perimeter() << endl;
+}
 };
